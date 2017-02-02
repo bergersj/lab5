a53c5ab2274509fd520bc4ae4efdf5b55ba8b544
bergersj <joshbergers35@gmail.com>
2017-02-02 14:32:51 -0500

    update

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ var tempFahrenheit = prompt('Enter temperature in Fahrenheit');
 var tempCelsius = ((tempFahrenheit - 32) * 5) /9;
 
 console.log('The temperature is '+ tempCelsius +' degrees Celsius.');
+
+var tempCelsius = prompt('Enter temperature in Celsius');
+var tempFahrenheit = ((tempCelsius *9) /5) +32;
+
+console.log('The temperature is '+ tempFahrenheit +' degrees Fahrenheit.');
