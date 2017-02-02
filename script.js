67af4f56498503fa174c07fe4c381be360851a60
bergersj <joshbergers35@gmail.com>
2017-02-02 14:25:31 -0500

    first commit lab5

---
+++ script.js
@@ -0,0 +1,4 @@
+var tempFahrenheit = prompt('Enter temperature in Fahrenheit');
+var tempCelsius = ((tempFahrenheit - 32) * 5) /9;
+
+console.log('The temperature is '+ tempCelsius +' degrees Celsius.');
